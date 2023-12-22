3c8762e331021200aabe0ed5bf94fa2b421dffd8
Aman Patel <aman17626@gmail.com>
2023-12-22 19:53:43 +0530

    updated navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import logo from "/logo.png";
+import { FiPhoneCall } from "react-icons/fi";
 
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Menu</summary>
+          <ul className="p-2">
+            <li>
+              <a>All</a>
+            </li>
+            <li>
+              <a>Salad</a>
+            </li>
+            <li>
+              <a>Pizza</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Services</summary>
+          <ul className="p-2">
+            <li>
+              <a>Online Order</a>
+            </li>
+            <li>
+              <a>Table Booking</a>
+            </li>
+            <li>
+              <a>Order Tracking</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Offers</a>
+      </li>
+    </>
+  );
   return (
     <header className="max-w-screen-2xl container mx-auto ">
       <div className="navbar xl:px-24">
@@ -27,23 +70,7 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navItems}
             </ul>
           </div>
           <a href="/">
@@ -51,30 +78,54 @@ const Navbar = () => {
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          {/* Search button */}
+          <button className="btn btn-ghost btn-circle hidden lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          {/* {cart items} */}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle mr-3 lg:flex hidden items-center justify-center"
+          >
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          {/* {btn} */}
+          <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-2">
+            <FiPhoneCall /> Contact
+          </a>
         </div>
       </div>
     </header>
