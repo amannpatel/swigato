f59b29a30922616a68eb86bb5a39f64b1eb4227b
Aman Patel <aman17626@gmail.com>
2023-12-22 21:11:21 +0530

    Add new images and update Banner styles

---
+++ src/components/Banner.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+        {/* Image */}
+        <div className="md:w-1/2">
+          <img src="/images/home/Banner.png" alt="" />
+          <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
+            <div className="flex bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
+              <img
+                src="/images/home/b-food1.png"
+                alt=""
+                className="rounded-2xl"
+              />
+              <div className="space-y-1">
+                <h5 className="font-medium mb-1">Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    checked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red">₹80.00</p>
+              </div>
+            </div>
+            <div className="sm:flex hidden bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
+              <img
+                src="/images/home/b-food1.png"
+                alt=""
+                className="rounded-2xl"
+              />
+              <div className="space-y-1">
+                <h5 className="font-medium mb-1">Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    checked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red">₹80.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Texts */}
+        <div className="md:w-1/2 space-y-7 px-4">
+          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+            Dive into Delights Of Delectable
+            <span className="text-green">Food</span>
+          </h2>
+          <p className="text-xl text-[#4A4A4A]">
+            Where Each Plate Weaves a Story of Culinary Mastery and Passionate
+            Craftsmanship
+          </p>
+          <button className="btn bg-green px-8 py-3 font-semibold text-white rounded-full">
+            Order Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
