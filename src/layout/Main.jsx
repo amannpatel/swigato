fdaa3e6666de71a554cf1c9622566f2dbcc7cdbe
Aman Patel <aman17626@gmail.com>
2023-12-24 09:41:32 +0530

    added footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../App.css";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
